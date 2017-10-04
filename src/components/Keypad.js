e44d712889bfdec3aa06356383edec30866e8196
John Vascsinec <pensteeler@gmail.com>
2017-10-04 19:58:04 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Keypad extends React.Component {
 
   keypad = () => {
-    console.log('Enter password.!');
+    console.log('Entering password...');
   }
 
   render() {
